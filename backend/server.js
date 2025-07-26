882063eb0d913a781dbe57a9caf449dce7358da6
Akshit Sood <akshitsod2013@gmail.com>
2025-07-27 03:54:04 +0530

    BE works now FE

--- backend/server.js
+++ backend/server.js
@@ -71,9 +71,9 @@ connectDB().then(() => {
 
 connectDB();
 
-// // Serve Frontend (If Deployed)
-// app.use(express.static(path.join(__dirname, "../frontend/build")));
+// Serve Frontend (If Deployed)
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+});
