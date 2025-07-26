25f42bcf9edf4a60f8e2543c75fcf86d006911c7
Akshit Sood <akshitsod2013@gmail.com>
2025-07-27 01:55:53 +0530

    small changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
+    console.log("Connecting to MongoDB..."),
+    console.log("Mongo URI:", process.env.MONGO_URI); // Log the MongoDB URI for debugging
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
